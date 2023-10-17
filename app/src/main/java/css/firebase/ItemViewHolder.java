75a5e96423ed12d95f513eb904cfe8d3600c253d
Tom Gibbons <tgibbons@css.edu>
2023-10-16 20:26:39 -0500

    Initial commit

---
+++ app/src/main/java/css/firebase/ItemViewHolder.java
@@ -0,0 +1,18 @@
+package css.firebase;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemViewHolder extends RecyclerView.ViewHolder{
+
+    TextView textViewRowItem;
+
+    public ItemViewHolder(@NonNull View itemView) {
+        super(itemView);
+        textViewRowItem = itemView.findViewById(R.id.textViewRowItem);
+    }
+}
+
